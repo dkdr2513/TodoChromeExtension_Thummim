97628408501a8c9a34536931d03a83a18ae7671e
dkdr2513 <50846877+dkdr2513@users.noreply.github.com>
2020-10-19 01:34:57 -0600

    commit

---
+++ precache-manifest.d8f41ecaaa282672357b3829b98d6fc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e142797038a91b9ed2bfdaff8b6bdaf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "210d180e6a3652958df4",
+    "url": "/static/css/main.278f2eb2.chunk.css"
+  },
+  {
+    "revision": "af3c666207ca497b8e48",
+    "url": "/static/js/2.7509fa9f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7509fa9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210d180e6a3652958df4",
+    "url": "/static/js/main.735d3621.chunk.js"
+  },
+  {
+    "revision": "c3bc1ce4a2d561e2a484",
+    "url": "/static/js/runtime-main.5745d301.js"
+  },
+  {
+    "revision": "e4c1f74e2bc812b5ac1843f685408941",
+    "url": "/static/media/bg.e4c1f74e.jpg"
+  }
+]);
